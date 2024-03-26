5fe5acb7a6d33489d8c531126ee4b7f2409aad07
jaketattersall <jaketattersall90@gmail.com>
2024-03-25 17:29:39 -0700

    Loading component 2.0 and changed value to false

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,14 +1,17 @@
 import React, { Component } from "react";
-
+import loading from "./loading";
 import classnames from "classnames";
 
 class Dashboard extends Component {
     state = {
-        loading: true
+        loading: false
       };
-      
+
   render() {
     const dashboardClasses = classnames("dashboard");
+    if (this.state.loading) {
+        return <loading />;
+      }
 
     return <main className={dashboardClasses} />;
   }
